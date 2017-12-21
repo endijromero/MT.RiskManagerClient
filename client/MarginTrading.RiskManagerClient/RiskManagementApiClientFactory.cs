129917e60a73465df81eb05c3f232528b37dbe4e
Nuno Araujo <nunobaraujo@hotmail.com>
2017-12-21 16:29:51 +0000

    Added test client and removed apikey from factory

--- client/MarginTrading.RiskManagerClient/RiskManagementApiClientFactory.cs
+++ client/MarginTrading.RiskManagerClient/RiskManagementApiClientFactory.cs
@@ -16,26 +16,13 @@ namespace MarginTrading.RiskManagerClient
         ///     <p>Setting the <paramref name="userAgent" /> is required to detect the requests source on the server side.
         ///     It shoud be set to the name of the appication using the api.</p>
         /// </summary>
-        public static IRiskManagementApi CreateDefaultClient(string apiUrl, string userAgent, string apiKey)
+        public static IRiskManagementApi CreateDefaultClient(string apiUrl, string userAgent)
         {
-            var client = new RiskManagementApi(new Uri(apiUrl), new ApiKeyHeaderAddingHandler(apiKey));
+            var client = new RiskManagementApi(new Uri(apiUrl));
             client.SetRetryPolicy(
                 new RetryPolicy<ErrorDetectionStrategy>(new FixedIntervalRetryStrategy(6, TimeSpan.FromSeconds(5))));
             client.SetUserAgent(userAgent);
             return client;
-        }
-        /// <summary>
-        /// <p>Creates the api clients pair with recomended customizations.</p>
-        /// <p>These customizations consist of: a retry policy to retry http errors 400 e 500 with a 5 seconds period, and max retry
-        /// count of 6.</p>
-        /// <p>Setting the <paramref name="userAgent" /> is required to detect the requests source on the server side.
-        /// It shoud be set to the name of the appication using the api.</p>
-        /// </summary>        
-        public static RiskManagementLiveDemoClientsPair CreateDefaultClientsPair(string demoApiUrl,
-            string liveApiUrl, string demoApiKey, string liveApiKey, string userAgent)
-        {
-            return new RiskManagementLiveDemoClientsPair(CreateDefaultClient(demoApiUrl, userAgent, demoApiKey),
-                CreateDefaultClient(liveApiUrl, userAgent, liveApiKey));
-        }
+        }       
     }
 }
---
+++ tests/MarginTrading.RiskManagerClient.ConsumerTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MarginTrading.RiskManagerClient.ConsumerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Task.Run(async () => await Tests()).Wait();
+        }
+
+        private static async Task Tests()
+        {
+            string key = "margintrading";
+            var service = RiskManagementApiClientFactory.CreateDefaultClient("http://mt-riskmanager.lykke-mt.svc.cluster.local/", "TestAgent");
+            var ivarlimits = await service.ApiIvarlimitGetAllGetAsync(key);
+            Console.WriteLine("ivarlimits={0}", ivarlimits.Count);
+            Console.WriteLine("Tests finished");
+            Console.ReadLine();
+
+        }
+    }
+}
